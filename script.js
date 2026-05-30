60cef5dea20ba5606e47f32dc85757cd6d5f8c40
Vinicius Araujo <90992850+ViniHADS@users.noreply.github.com>
2026-05-29 21:18:11 -0300

    commit 29/05
    Está funcionando o evento da analise comparativa e individual;
    precisamos terminar a lógica bloco do IF;
    
    Co-Authored-By: devgabrieldonato <266819092+devgabrieldonato@users.noreply.github.com>
    Co-Authored-By: Jorge Augusto Flores Meert <250581622+jorgemeert@users.noreply.github.com>

--- script.js
+++ script.js
@@ -18,8 +18,17 @@ map.on("click", (event) => {
 
   coordsText.textContent = `Latitude: ${latFixed} | Longitude: ${lngFixed}`;
 
-  // Preenche o input de latitude ao clicar no mapa
-  document.getElementById("latitude").value = latFixed;
+  //Voltar apartir daqui commit 29/05/26!!
+  
+  //Preenchimento Latitude base 1 & Latitude base 2 - Analíse Comparativa
+  latitude = document.getElementById("latitude"); //Definido a constante para poder ser utilizada no evento exclusivo do analíse comparativa
+  if (latitude.classList.contains("dadosComp")) {
+    console.log('oi1');
+  }else{
+    // Preenche o input de latitude ao clicar no mapa
+  document.getElementById("latitude").value = latFixed;  
+  console.log("oi2");
+  }
 
   if (marker) {
     marker.setLatLng([lat, lng]);
@@ -32,9 +41,10 @@ map.on("click", (event) => {
 
 // Lógica de validação Latitude
 const btnAnalisar = document.getElementById("botao");
+let latitude = document.getElementById("latitude");
 
 btnAnalisar.addEventListener("click", () => {
-  let latitude = document.getElementById("latitude").value;
+  latitude = document.getElementById("latitude").value;
   latitude = parseFloat(latitude);
   if (latitude < -90 || latitude > 90) {
     document.getElementById("latitude").value = "";
@@ -43,4 +53,19 @@ btnAnalisar.addEventListener("click", () => {
     document.querySelector("#erro").classList.add("erro");
     document.getElementById("latitude").classList.add("erroCaixa");
   }
+});
+
+// Case #2 da história, matéria UX
+// Análise comparativa
+const btnComp = document.getElementById("btnComp");
+const btnInd = document.getElementById("btnInd");
+
+btnComp.addEventListener("click", () => {
+  document.querySelectorAll(".dadosInd").forEach(e => e.remove());
+  document.querySelector(".dados").innerHTML = "<label id='base1' class='dadosComp'>Nome Base 1</label> <input type='text' class='dadosComp'> <label class='dadosComp'>Latitude base 1</label> <input id='latitude' type='text' class='dadosComp'> <label id='base2' class='dadosComp'>Nome Base 2</label> <input type='text' class='dadosComp'> <label id='lat2' class='dadosComp'>Latitude base 2</label> <input type='text' class='dadosComp'> <button class='dadosComp'>Analisar lançamento</button>"
+});
+
+btnInd.addEventListener("click", () => {
+  document.querySelectorAll(".dadosComp").forEach(e => e.remove());
+  document.querySelector(".dados").innerHTML = "<label id='base' class='dadosInd'>Nome Base</label> <input type='text' class='dadosInd'> <label id='lat' class='dadosInd'>Latitude</label> <input id='latitude' type='text' class='dadosInd'> <button class='dadosInd'>Analisar lançamento</button>"
 });
