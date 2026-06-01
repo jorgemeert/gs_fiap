a1810403f2dd76ba939f8d5024ae8227b7f2f53d
Jorge Augusto Flores Meert <jorgeflmrttt@icloud.com>
2026-06-01 18:59:58 -0300

    Commit quase final
    Commit quase final

--- script.js
+++ script.js
@@ -52,14 +52,14 @@ const btnInd = document.getElementById("btnInd");
 btnComp.addEventListener("click", () => {
   document.querySelectorAll(".dadosInd").forEach((e) => e.remove());
   document.querySelector(".dados").innerHTML =
-    "<label for='base' class='dadosComp'>Nome Base 1</label> <input type='text' id='base' class='dadosComp'> <label for='latitude' class='dadosComp'>Latitude base 1</label> <input id='latitude' type='text' class='dadosComp'> <label for='base2' class='dadosComp'>Nome Base 2</label> <input type='text' id='base2' class='dadosComp'> <label for='lat2' class='dadosComp'>Latitude base 2</label> <input id='lat2' type='text' class='dadosComp'> <button class='dadosComp' id='botao'>Analisar lançamento</button>  <div id='erro' class='dadosComp'></div>";
+    "<div class='analiseComp'><label for='base' class='dadosComp'>Nome Base 1</label> <input type='text' id='base' class='dadosComp'> <label for='latitude' class='dadosComp'>Latitude base 1</label> <input id='latitude' type='text' class='dadosComp'> <label for='base2' class='dadosComp'>Nome Base 2</label> <input type='text' id='base2' class='dadosComp'> <label for='lat2' class='dadosComp'>Latitude base 2</label> <input id='lat2' type='text' class='dadosComp'> <button class='dadosComp' id='botao'>Analisar lançamento</button>  <div id='erro' class='dadosComp'></div>";
 });
 
 //Ação ao trocar para Análise Indivudal
 btnInd.addEventListener("click", () => {
   document.querySelectorAll(".dadosComp").forEach((e) => e.remove());
   document.querySelector(".dados").innerHTML =
-    "<label for='base' class='dadosInd'>Nome da Base</label><input id='base' type='text' class='dadosInd' /><label for='latitude' class='dadosInd'>Latitude</label><input id='latitude' type='text' class='dadosInd' /><button id='botao' class='dadosInd'>Analisar Lançamento</button><div id='erro' class='dadosInd'></div>";
+    "<div class='analiseInd'><label for='base' class='dadosInd'>Nome da Base</label><input id='base' type='text' class='dadosInd' /><label for='latitude' class='dadosInd'>Latitude</label><input id='latitude' type='text' class='dadosInd' /><button id='botao' class='dadosInd'>Analisar Lançamento</button><div id='erro' class='dadosInd'></div>";
 });
 
 const btnAnalisar = document.getElementById("botao");
@@ -72,6 +72,7 @@ dados.addEventListener("click", (event) => {
     latitude = document.getElementById("latitude").value;
     latitude = parseFloat(latitude);
     const base = document.getElementById("base").value;
+    let erros = 0;
 
     if (latitude < -90 || latitude > 90 || !latitude) {
       document.getElementById("latitude").value = "";
@@ -79,6 +80,7 @@ dados.addEventListener("click", (event) => {
         "<p>Latitude Base 1 inválida </p>";
       document.querySelector("#erro").classList.add("erro");
       document.getElementById("latitude").classList.add("erroCaixa");
+      erros = erros + 1;
     } else if (document.querySelector("#lat2")) {
       const latitude2 = document.getElementById("lat2").value;
       if (latitude2 < -90 || latitude2 > 90 || !latitude2) {
@@ -87,6 +89,7 @@ dados.addEventListener("click", (event) => {
           "<p>Latitude Base 2 inválida </p>";
         document.querySelector("#erro").classList.add("erro");
         document.getElementById("lat2").classList.add("erroCaixa");
+        erros = erros + 1;
       }
     }
     if (base.length === 0 || base.length < 2) {
@@ -94,6 +97,7 @@ dados.addEventListener("click", (event) => {
       document.querySelector("#erro").innerHTML =
         "<p>Digite na base 1 um nome válido </p>";
       document.querySelector("#erro").classList.add("erro");
+      erros = erros + 1;
     } else if (document.querySelector("#lat2")) {
       const base2 = document.getElementById("base2").value;
       if (base2.length === 0 || base2.length < 2) {
@@ -101,11 +105,12 @@ dados.addEventListener("click", (event) => {
         document.querySelector("#erro").innerHTML =
           "<p>Digite na base 2 um nome válido </p>";
         document.querySelector("#erro").classList.add("erro");
+        erros = erros + 1;
       }
     }
 
-    function calcularEnergia(latitudeGraus) {
-      const phi = latitudeGraus * (Math.PI / 180);
+    function calcularEnergia(latitude) {
+      const phi = latitude * (Math.PI / 180);
 
       const R_T = 6371000;
       const r_GEO = 42164000;
@@ -117,19 +122,62 @@ dados.addEventListener("click", (event) => {
         0.5 * 1000 * Math.pow(v_GEO - omega * R_T * Math.cos(phi), 2) +
         ((1000 * mu) / 2) * (1 / R_T - 1 / r_GEO);
 
-      return energia;
+      const ganhoRotacao = omega * R_T * Math.cos(phi);
+
+      let eficiencia = "";
+
+      if (energia > 420) {
+        eficiencia = "Eficiência: Perfeita";
+      } else if (energia > 350) {
+        eficiencia = "Eficiência: Alta";
+      } else if (energia > 200) {
+        eficiencia = "Eficiência: Média";
+      } else if (energia > 100) {
+        eficiencia = "Eficiência: Baixa";
+      } else {
+        eficiencia = "Eficiência: Muito Baixa";
+      }
+
+      return [energia, ganhoRotacao, eficiencia];
     }
 
     function compararLatitudes(lat1, lat2) {
       const e1 = calcularEnergia(lat1);
       const e2 = calcularEnergia(lat2);
       const economia = e1 - e2;
-      return { e1, e2, economia };
-    }
-    // CONTINUAR DAQUI, LÓGICA, APRESENTAR RESULTADO.
 
-    document.querySelectorAll(".dadosInd").forEach((e) => e.remove());
-    document.querySelector(".dados").innerHTML = "<h2>Resultado</h2> <p id='base'> </p> <p id='latitude'> </p><h2>Energia estimada</h2><p id='energia'></p><p>Ganho rotação</p> <p id='rotacao'></p>";
-    console.log("passou Aqui");
+      return economia;
+    }
+
+    if (erros == 0) {
+      if (document.querySelector("#lat2")) {
+        const latitude2 = document.getElementById("lat2").value;
+        const base2 = document.getElementById("base2").value;
+        const economia = compararLatitudes(latitude, latitude2);
+        const [energia1, ganhoRotacao1, eficiencia1] =
+          calcularEnergia(latitude);
+        const [energia2, ganhoRotacao2, eficiencia2] =
+          calcularEnergia(latitude);
+
+        document.querySelectorAll(".dadosComp").forEach((e) => e.remove());
+        document.querySelector(".btnTroca").remove();
+        document.querySelector(".dados").innerHTML =
+          `<div> <h2>Resultado</h2><div class='resultComp'><div class='base1'><p>Base 1</p><p id='base'>Base: ${base}</p> <p id='latitude'>Latitude: ${latitude}</p><p>Energia estimada</p><p>${energia1.toFixed(2)} MJ/t</p> <p>Ganho de rotação</p> <p>${ganhoRotacao1.toFixed(2)} m/s</p><p>${eficiencia1}</p></div>
+          <div class='base2'><p>Base 2</p><p id='base2'>Base: ${base2} </p> <p id='latitude2'>Latitude:${latitude2} </p><p>Energia estimada</p><p>${energia2.toFixed(2)} MJ/t</p> <p>Ganho de rotação</p> <p>${ganhoRotacao2.toFixed(2)} m/s</p><p>${eficiencia1}</p></div></div> <button id='btnReset'>Nova análise</button>`;
+        console.log(ganhoRotacao2);
+        btnReset.addEventListener("click", () => {
+          location.reload();
+        });
+      } else {
+        const [energia, ganhoRotacao, eficiencia] = calcularEnergia(latitude);
+        document.querySelectorAll(".dadosInd").forEach((e) => e.remove());
+        document.querySelector(".btnTroca").remove();
+        document.querySelector(".dados").innerHTML =
+          `<div class='resultInd'><h2>Resultado</h2> <p id='base'>${base}</p> <p id='latitude'>${latitude}</p><h2>Energia estimada</h2><p id='energia'>${energia.toFixed(2)} MJ/t</p><p>Ganho rotação</p> <p id='rotacao'>${ganhoRotacao.toFixed(2)} m/s</p> <p>${eficiencia}</p> <button id='btnReset'>Nova análise</button></div>`;
+        btnReset.addEventListener("click", () => {
+          location.reload();
+        });
       }
+    }
+  }
 });
