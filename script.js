aba43bafac96ef82d8697bb4dc8e05e32465ba10
Jorge Augusto Flores Meert <jorgeflmrttt@icloud.com>
2026-06-01 13:15:37 -0300

    ultimo commit
    ultimo commit

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ const btnInd = document.getElementById("btnInd");
 btnComp.addEventListener("click", () => {
   document.querySelectorAll(".dadosInd").forEach((e) => e.remove());
   document.querySelector(".dados").innerHTML =
-    "<label id='base1' class='dadosComp'>Nome Base 1</label> <input type='text' class='dadosComp'> <label class='dadosComp'>Latitude base 1</label> <input id='latitude' type='text' class='dadosComp'> <label id='base2' class='dadosComp'>Nome Base 2</label> <input type='text' class='dadosComp'> <label class='dadosComp'>Latitude base 2</label> <input id='lat2' type='text' class='dadosComp'> <button class='dadosComp' id='botao'>Analisar lançamento</button> <div id='erro' class='dadosComp'></div>";
+    "<label for='base' class='dadosComp'>Nome Base 1</label> <input type='text' id='base' class='dadosComp'> <label for='latitude' class='dadosComp'>Latitude base 1</label> <input id='latitude' type='text' class='dadosComp'> <label for='base2' class='dadosComp'>Nome Base 2</label> <input type='text' id='base2' class='dadosComp'> <label for='lat2' class='dadosComp'>Latitude base 2</label> <input id='lat2' type='text' class='dadosComp'> <button class='dadosComp' id='botao'>Analisar lançamento</button>  <div id='erro' class='dadosComp'></div>";
 });
 //Ação ao trocar para Análise Indivudal
 btnInd.addEventListener("click", () => {
@@ -70,15 +70,64 @@ dados.addEventListener("click", () => {
   if (event.target.id === "botao") {
     latitude = document.getElementById("latitude").value;
     latitude = parseFloat(latitude);
+    const base = document.getElementById("base").value;
 
-    if (latitude < -90 || latitude > 90) {
+    if (latitude < -90 || latitude > 90 || !latitude) {
       document.getElementById("latitude").value = "";
-      document.querySelector("#erro").innerHTML = "<p>Latitude inválida </p>";
+      document.querySelector("#erro").innerHTML =
+        "<p>Latitude Base 1 inválida </p>";
       document.querySelector("#erro").classList.add("erro");
       document.getElementById("latitude").classList.add("erroCaixa");
+    } else if (document.querySelector("#lat2")) {
+      const latitude2 = document.getElementById("lat2").value;
+      if (latitude2 < -90 || latitude2 > 90 || !latitude2) {
+        document.getElementById("lat2").value = "";
+        document.querySelector("#erro").innerHTML =
+          "<p>Latitude Base 2 inválida </p>";
+        document.querySelector("#erro").classList.add("erro");
+        document.getElementById("lat2").classList.add("erroCaixa");
+      }
+    }
+    if (base.length === 0 || base.length < 2) {
+      document.getElementById("base").classList.add("erroCaixa");
+      document.querySelector("#erro").innerHTML =
+        "<p>Digite na base 1 um nome válido </p>";
+      document.querySelector("#erro").classList.add("erro");
+    } else if (document.querySelector("#lat2")) {
+      const base2 = document.getElementById("base2").value;
+      if (base2.length === 0 || base2.length < 2) {
+        document.getElementById("base2").classList.add("erroCaixa");
+        document.querySelector("#erro").innerHTML =
+          "<p>Digite na base 2 um nome válido </p>";
+        document.querySelector("#erro").classList.add("erro");
+      }
     }
 
-    // Contiunar daqui, ultimo commit 29/05 - 23:00
-    // Falta a lógica do erro aparecer na longitude base 2 da análise comparativa.
+    function calculoEnergia(latitude) {
+      const phi = latitudeGraus * (Math.PI / 180);
+
+      const R_T = 6371000;
+      const r_GEO = 42164000;
+      const v_GEO = 3070;
+      const omega = 7.2921159e-5;
+      const mu = 3.986004418e14;
+
+      const energia =
+        0.5 * 1000 * Math.pow(v_GEO - omega * R_T * Math.cos(phi), 2) +
+        ((1000 * mu) / 2) * (1 / R_T - 1 / r_GEO);
+
+      return energia;
+    }
+
+    function compararLatitudes(lat1, lat2) {
+      const e1 = calcularEnergia(lat1);
+      const e2 = calcularEnergia(lat2);
+      const economia = e1 - e2;
+      return { e1, e2, economia };
+    }
+
+    // CONTINUAR DAQUI, LÓGICA, APRESENTAR RESULTADO.
+
+    //document.getElementById("dados").innerHTML = "<h2>Resultado</h2><p id='base'></p><p id='latitude'></p><h2>Energia estimada</h2><p id='energia'></p><p>Ganho rotação</p> <p id='rotacao'></p> " ;
   }
 });
